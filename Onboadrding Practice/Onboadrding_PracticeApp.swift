dedd767caa2815127245bdf117bc4f8348b14724
Tariq Almazyad <tariq.almazyad@gmail.com>
2020-11-08 01:00:30 +0300

    Initial Commit

---
+++ Onboadrding Practice/Onboadrding_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onboadrding_PracticeApp.swift
+//  Onboadrding Practice
+//
+//  Created by Tariq Almazyad on 11/7/20.
+//
+
+import SwiftUI
+
+@main
+struct Onboadrding_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
